dea90cebdf0f4362a047e775bf1137fe4d9fb85e
FabioAJ <fabioaugusto.aj@gmail.com>
2018-12-14 14:57:43 -0200

    Cria uma fatura que pode ser modificada com metodos set e get

---
+++ Fatura/Fatura.java
@@ -0,0 +1,57 @@
+/*
+Nome do autor: Fábio Augusto Amaro Jaime
+Data de criação do arquivo: 14/12/2018
+Objetivo sucinto do programa: Cria uma fatura que pode ser modificada com metodos set e get.
+Referência ao enunciado/origem do exercício: https://docs.google.com/document/d/1Uq6IUqO100w2ylIxVi17RTwYLo-ljPu2YGu7KPPDydg/edit
+*/
+
+public class Fatura {
+
+    private int id;
+    private String descricao;
+    private int quantidade;
+    private double precoUnidade;
+
+    public Fatura() {
+
+    }
+
+    public Fatura(int id, String descricao, int quantidade, double precoUnidade) {
+        this.id = id;
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+        this.precoUnidade = precoUnidade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = this.quantidade + quantidade;
+    }
+
+    public double getPrecoUnidade() {
+        return precoUnidade;
+    }
+
+    public void setPrecoUnidade(double precoUnidade) {
+        this.precoUnidade = precoUnidade;
+    }
+
+    public double getPrecoTotal() {
+        return precoUnidade * quantidade;
+    }
+
+    public String toString() {
+        return "id do produto = " + id + " descricao = " + descricao + " quantidade = " + quantidade + " preco da unidade = " + precoUnidade + "\n";
+    }
+}
---
+++ Fatura/TestaFatura.java
@@ -0,0 +1,30 @@
+/*
+Nome do autor: Fábio Augusto Amaro Jaime
+Data de criação do arquivo: 14/12/2018
+Objetivo sucinto do programa: Cria uma fatura que pode ser modificada com metodos set e get.
+Referência ao enunciado/origem do exercício: https://docs.google.com/document/d/1Uq6IUqO100w2ylIxVi17RTwYLo-ljPu2YGu7KPPDydg/edit
+*/
+
+public class TestaFatura {
+
+    public static void main(String[] args) {
+        Fatura produto1 = new Fatura(123, "pao de batata", 22, 20);
+        System.out.println(produto1.toString());
+
+        Fatura produto2 = new Fatura(456, "pao frances", 12, 28);
+        System.out.println(produto2.toString());
+
+        Fatura produto3 = new Fatura(789, "baguete", 44, 42);
+        System.out.println(produto3.toString());
+
+        System.out.println("o id do baguete é: " + produto3.getId());
+        System.out.println("descricao do produto 2: " + produto2.getDescricao());
+        System.out.println("ha em estoque " + produto2.getQuantidade() + " paes fanceses");
+        produto2.setQuantidade(23);
+        System.out.println("agora ha em estoque " + produto2.getQuantidade() + " paes franceses");
+        System.out.println("cada unidade do pao de batata esta saindo por R$" + produto1.getPrecoUnidade());
+        produto1.setPrecoUnidade(15);
+        System.out.println("a unidade do pao de batata entrou em promocao agora esta R$" + produto1.getPrecoUnidade());
+        System.out.println("agora o lote do pao de batata esta R$" + produto1.getPrecoTotal());
+    }
+}
