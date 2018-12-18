5b370d4b2da76d2469c1a5b6be15baf4695e6a0a
FabioAJ <fabioaugusto.aj@gmail.com>
2018-12-18 15:34:08 -0200

    Adiciona comentario padrao para as classes ContaBancaria e TestaContaBancaria

--- ContaBancaria/ContaBancaria.java
+++ ContaBancaria/ContaBancaria.java
@@ -1,3 +1,10 @@
+/*
+Nome do autor: Fábio Augusto Amaro Jaime
+Data de criação do arquivo: 14/12/2018
+Objetivo sucinto do programa: Cria contas bancarias que pode ser modificadas com metodos set e get.
+Referência ao enunciado/origem do exercício: https://docs.google.com/document/d/1Uq6IUqO100w2ylIxVi17RTwYLo-ljPu2YGu7KPPDydg/edit
+*/
+
 public class ContaBancaria {
 
     private int id;
