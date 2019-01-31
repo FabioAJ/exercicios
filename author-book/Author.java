2afcca5e6f67b298d97fad75ab442b663d6ac6bf
FabioAJ <fabioaugusto.aj@gmail.com>
2019-01-31 17:47:54 -0200

    Mostra exemplo de equals

--- author-book/Author.java
+++ author-book/Author.java
@@ -2,7 +2,7 @@
 Nome do autor: Fábio Augusto Amaro Jaime
 Data de criação do arquivo: 23/01/2019
 Objetivo sucinto do programa: cria uma classe Author
-Referência ao enunciado/origem do exercício: https://docs.google.com/document/d/1wSPKmVGsvzHlM13SY_IxCcqA1AhFboDvSy6M6NqOTfY/edit
+Referência ao enunciado/origem do exercício: https://docs.google.com/document/d/1Znd6TTkM0lh0M9vUTP1yVve8KSKEn8TlKzfJf8zyX9U/edit 
 */
 
 public class Author {
@@ -37,5 +37,21 @@ public class Author {
 	public String toString() {
 		return "name = " + name + ", email = " + email + ", gender = " + gender;
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Author other = (Author) obj;
+		if (name.equalsIgnoreCase(other.getName()) && email.equalsIgnoreCase(other.getEmail())) {
+			return true;
+		}
+		return false;
+	}
+	
 
 }
--- author-book/Book.java
+++ author-book/Book.java
@@ -2,7 +2,7 @@
 Nome do autor: Fábio Augusto Amaro Jaime
 Data de criação do arquivo: 23/01/2019
 Objetivo sucinto do programa: Cria uma classe Book
-Referência ao enunciado/origem do exercício: https://docs.google.com/document/d/1wSPKmVGsvzHlM13SY_IxCcqA1AhFboDvSy6M6NqOTfY/edit
+Referência ao enunciado/origem do exercício: https://docs.google.com/document/d/1Znd6TTkM0lh0M9vUTP1yVve8KSKEn8TlKzfJf8zyX9U/edit 
 */
 
 public class Book {
